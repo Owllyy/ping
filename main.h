be5bd30b9b2706600aea8985f69d3333b6fb1ce2
Bourret Lucas <bourretlucas@gmail.com>
2024-02-07 12:31:29 +0100

    Icmp + Ip

---
+++ main.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+ 
